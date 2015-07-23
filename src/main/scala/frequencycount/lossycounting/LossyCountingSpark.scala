ae1b3e8bc2c6b6dac8fae86c0f3801a0f2cb5692
Michael Vogiatzis <michaelvogiatzis@gmail.com>
2015-07-23 14:59:00 +0100

    Added spark classes for both algorithms

---
+++ src/main/scala/frequencycount/lossycounting/LossyCountingSpark.scala
@@ -0,0 +1,129 @@
+package frequencycount.lossycounting
+
+import frequencycount.Item
+
+import scala.collection.parallel.mutable
+
+object LossyCountingSpark {
+
+  /**
+   * Simulation of the window input data
+   */
+  val itemBatches = List[List[String]](
+
+    List.concat(create(19, Item.Red), create(11, Item.Blue), create(10, Item.Yellow), create(10, Item.Brown), create(0, Item.Green)),
+    List.concat(create(30, Item.Red), create(10, Item.Blue), create(10, Item.Yellow)),
+    List.concat(create(30, Item.Red), create(10, Item.Blue), create(0, Item.Yellow), create(5, Item.Brown), create(5, Item.Green)),
+    List.concat(create(40, Item.Red), create(10, Item.Blue)),
+    List.concat(create(40, Item.Red), create(10, Item.Blue))
+  )
+
+  def main(args: Array[String]): Unit = {
+    val frequency = 0.2
+    val error = 0.1 * frequency
+    val windowSize = 1.0 / error
+
+    val sc = initSparkContext()
+    val frequencyVal = sc.broadcast(frequency)
+    val errorVal = sc.broadcast(error)
+    val streamingContext = new StreamingContext(sc, Seconds(1))
+    streamingContext.checkpoint("./checkpoint/")
+    val partitioner = new HashPartitioner(streamingContext.sparkContext.defaultParallelism)
+    var totalElements = 0L
+
+    val rddQueue = new mutable.SynchronizedQueue[RDD[String]]()
+    val inputDStream = streamingContext.queueStream(rddQueue)
+
+    //update window on driver
+    inputDStream.foreachRDD{rdd =>
+      val rddCount = rdd.count()
+      totalElements += rddCount
+    }
+
+    val items: DStream[String] = inputDStream.flatMap(line => line.split(" "))
+    val itemOneValuePairs: DStream[(String, Int)] = items.map(item => (item, 1))
+    val countPerItem: DStream[(String, Int)] = itemOneValuePairs.reduceByKey((count1, count2) => count1 + count2)
+
+    val updatedState = countPerItem.updateStateByKey(updateFrequencyByKeyAndDecr _, partitioner)
+
+    //keep only the items that exceed the threshold given by the Lossy Counting algorithm
+    val output = updatedState.filter{
+      itemWithCounts =>
+        println(s"Total elements $totalElements, predicate: ${frequencyVal.value * totalElements - errorVal.value * totalElements}")
+        itemWithCounts._2.toDouble > (frequencyVal.value * totalElements - errorVal.value * totalElements)
+    }
+    output.print()
+    println("Processing")
+
+
+
+
+    streamingContext.start() // Start the computation
+
+    // Create and push some RDDs into the
+    for (i <- itemBatches.indices) {
+      rddQueue += streamingContext.sparkContext.makeRDD(itemBatches(i))
+      Thread.sleep(1000)
+    }
+    println(s"Frequency: $frequency, Error: $error, Window size: $windowSize")
+    val labelWithCounts = calcTrueCounts(itemBatches)
+    printTrueCounts(labelWithCounts)
+
+    streamingContext.stop()
+  }
+
+  def updateFrequencyByKeyAndDecr(newValues: Seq[Int], currentCount: Option[Int]): Option[Int] = {
+    val sum: Int = newValues.sum
+    val aggregate: Int = currentCount match {
+      case Some(count) =>
+        count + sum
+      case None =>
+        sum
+    }
+    val decrementedValue = aggregate - 1
+    if (itemShouldBeEliminated(decrementedValue)) {
+      None //Spark will discard this key
+    } else {
+      Some(decrementedValue)
+    }
+  }
+
+
+  def printTrueCounts(labelWithCounts: mutable.HashMap[String, Int]) = {
+    val totalElements = labelWithCounts.foldLeft(0)(_ + _._2)
+    println(s"$totalElements total items")
+    println("True Counts")
+    val df = new DecimalFormat("#.##")
+    for (labelWithCount <- labelWithCounts) {
+      val trueCount = labelWithCount._2
+      val trueFreq = calcFreq(trueCount, totalElements)
+      println(s"${labelWithCount._1} - ${df.format(trueFreq)} % with true count $trueCount")
+    }
+  }
+
+  def calcFreq(count: Int, totalElements: Int) = {
+    (count.toDouble / totalElements.toDouble) * 100.0
+  }
+
+  def calcTrueCounts(itemsList: List[List[String]]): mutable.HashMap[String, Int] = {
+    val map = new mutable.HashMap[String, Int]
+    itemsList.foreach { coloursList =>
+      coloursList.foreach { colour =>
+        val count = map.getOrElse(colour, 0)
+        map.put(colour, count + 1)
+      }
+    }
+    map
+  }
+
+  private def itemShouldBeEliminated(decrementedValue: Int): Boolean = {
+    decrementedValue <= 0
+  }
+
+  def initSparkContext(): SparkContext = {
+    val conf = new SparkConf().setMaster("local[2]").setAppName("Lossy_Counting")
+    new SparkContext(conf)
+  }
+
+
+}
---
+++ src/main/scala/frequencycount/stickysampling/StickySamplingSpark.scala
@@ -0,0 +1,199 @@
+package frequencycount.stickysampling
+
+import java.util.concurrent.ThreadLocalRandom
+
+import frequencycount.Item
+
+object StickySamplingSpark {
+
+  //f = 0.2, e=0.02, d = 0.002
+  // t = (1/e) * log(1/(s*d))
+  // e = 50 * log (1/(0.2*0.002))
+
+
+  /**
+   * Frequency above which we want to print out frequent items
+   */
+  val frequency = 0.5
+
+  /**
+   * output = f*N - e*N, where N is the total number of elements
+   */
+  val error = 0.2 * frequency
+
+  /**
+   * t = (1/error) * log(1/(frequency * probabilityOfFailure))
+   * probabilityOfFailure affects the number of elements before changing the sampling rate
+   */
+  val probabilityOfFailure = 0.1 * error
+
+  /**
+   * The first t elements are sampled at rate r=1, the next 2t are sampled at rate r=2, the next 4t at r=4 and so on
+   */
+  val t = (1.0 / error) * Math.log(1.0 / (frequency * probabilityOfFailure))
+
+  val memCacheDB = new MemCacheDB[String, Int]
+
+  val RATE = "rate"
+  val INITIAL_SAMPLING_RATE = 1
+
+  /**
+   * Simulation of the input data assigned with a numeric id
+   */
+  val itemBatches: List[List[ItemWithId]] = {
+    val itemset = List[List[String]](
+      List.concat(create(19, Item.Red), create(11, Item.Blue), create(10, Item.Yellow), create(10, Item.Brown), create(0, Item.Green)),
+      List.concat(create(30, Item.Red), create(10, Item.Blue), create(10, Item.Yellow)),
+      List.concat(create(30, Item.Red), create(10, Item.Blue), create(0, Item.Yellow), create(5, Item.Brown), create(5, Item.Green)),
+      List.concat(create(40, Item.Red), create(10, Item.Blue)),
+      List.concat(create(40, Item.Red), create(10, Item.Blue))
+    )
+    var orderId = 0L
+    for (currList <- itemset) yield {
+      for (str <- currList) yield {
+        orderId += 1L
+        ItemWithId(str, orderId)
+      }
+    }
+  }
+
+
+  def main(args: Array[String]): Unit = {
+
+    println(s"Frequency: $frequency, Error: $error, Probability of failure: $probabilityOfFailure, t=$t")
+    val sc = initSparkContext()
+    val frequencyVal = sc.broadcast(frequency)
+    val errorVal = sc.broadcast(error)
+
+    var totalElements = 0L
+
+    val streamingContext = new StreamingContext(sc, Seconds(1))
+    streamingContext.checkpoint("./checkpoint/")
+    val partitioner = new HashPartitioner(streamingContext.sparkContext.defaultParallelism)
+
+    val rddQueue = new mutable.SynchronizedQueue[RDD[ItemWithId]]()
+    val inputDStream = streamingContext.queueStream(rddQueue)
+
+    inputDStream.foreachRDD(rdd => totalElements += rdd.count())
+
+
+    val itemsWithCount = inputDStream.map(itemWithId => (itemWithId.item, ItemCountRate(itemWithId.id, memCacheDB.get(RATE).getOrElse(INITIAL_SAMPLING_RATE), 0)))
+
+    val updatedState: DStream[(String, ItemCountRate)] = itemsWithCount.updateStateByKey(updateStateBySampling _, partitioner)
+
+    val output = updatedState.filter{
+      itemWithCounts =>
+        println(s"Total elements $totalElements, predicate: ${frequencyVal.value * totalElements - errorVal.value * totalElements}")
+        itemWithCounts._2.currentCount.toDouble > (frequencyVal.value * totalElements - errorVal.value * totalElements)
+    }
+    output.print()
+
+    streamingContext.start() // Start the computation
+
+    // push some RDDs
+    for (i <- itemBatches.indices) {
+      rddQueue += streamingContext.sparkContext.makeRDD(itemBatches(i))
+      Thread.sleep(1000)
+    }
+
+    streamingContext.stop()
+
+  }
+
+
+  def samplingRateHasChanged(currentSamplingRate: Int, existingStateOpt: Option[ItemCountRate]): Boolean = {
+    existingStateOpt match {
+      case Some(existingState) =>
+        currentSamplingRate > existingState.rate
+      case None =>
+        //no prev state exists, so sampling rate has not changed
+        false
+    }
+  }
+
+  def lowerIdFirstComparator(item1: ItemCountRate, item2: ItemCountRate): Boolean ={
+    item1.itemId < item2.itemId
+  }
+
+  def updateStateBySampling(newItems: Seq[ItemCountRate], currentCountOpt: Option[ItemCountRate]): Option[ItemCountRate] = {
+
+    //sort new values to preserve any order
+    val valuesSorted = newItems.sortWith(lowerIdFirstComparator)
+
+    newItems.foldLeft(currentCountOpt){ (B, listItem) =>
+      val currSamplingRate = SamplingRateRetriever.deriveSamplingRate(listItem.itemId, t)
+      val newCount = B match {
+        case Some(currentCount) =>
+          currentCount.currentCount + 1
+
+        case None => //item does not exist
+          getNewCountWithSampling(B, listItem, currSamplingRate)
+      }
+
+      if (newCount > 0){
+        if (samplingRateHasChanged(currSamplingRate, B)){
+          println(s"Sampling rate has changed: $currSamplingRate")
+          memCacheDB.put(RATE, currSamplingRate)
+          //toss a coin and for each unsuccessful shot, drop new count by one until successful or until 0
+          dropCountByTossingCoins(newCount) match {
+            case Some(reducedCount) =>
+              Some(ItemCountRate(listItem.itemId, currSamplingRate, reducedCount))
+            case None =>
+              None
+          }
+        }else{
+          Some(ItemCountRate(listItem.itemId, currSamplingRate, newCount))
+        }
+      } else{
+        None
+      }
+
+    }
+
+  }
+
+  def getCurrentCount(currentCountOpt: Option[ItemCountRate]): Long = {
+    currentCountOpt match {
+      case Some(count) =>
+        count.currentCount
+      case None =>
+        0L
+    }
+  }
+
+  def getNewCountWithSampling(B: Option[ItemCountRate], listItem: ItemCountRate, samplingRate: Int): Long = {
+    val countToAdd = if (canSelectItWithSamplingRate(samplingRate)) 1L else 0
+    B match {
+      case Some(existingItem) =>
+        existingItem.currentCount + countToAdd
+      case None =>
+        countToAdd
+    }
+  }
+
+  def canSelectItWithSamplingRate(samplingRate: Int): Boolean = {
+    ThreadLocalRandom.current().nextDouble() < (1.0 / samplingRate)
+  }
+
+
+
+  def dropCountByTossingCoins(newCount: Long): Option[Long] = {
+    var countAfterSampling = newCount
+    while(countAfterSampling > 0 && unsuccessfulCoinToss()){
+      countAfterSampling -= 1
+    }
+    if (countAfterSampling>0) Some(countAfterSampling) else None
+  }
+
+  def unsuccessfulCoinToss(): Boolean = {
+    ThreadLocalRandom.current().nextDouble() < 0.5
+  }
+
+  def initSparkContext(): SparkContext = {
+    val conf = new SparkConf().setMaster("local[2]").setAppName("Sticky Sampling")
+    new SparkContext(conf)
+  }
+
+
+
+}
